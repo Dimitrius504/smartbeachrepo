ef04ab4813aa3fbc63a5a366cc1b4225478fc152
Dimitrius Mckinnon <dimitriusmckinnon419@gmail.com>
2024-03-27 13:23:45 -0400

    Commit of Front-end team project files

--- smartbeach-project-prototype/src/App.js
+++ smartbeach-project-prototype/src/App.js
@@ -1,28 +1,32 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-// import Conditions from './components/Condition';
-import WeatherComponent from './components/Weather';
-import ApiComponent from './components/Kincardine';
-// import Home from './pages/Home';
-// import Forecast from './pages/Forecast';
+import './Forecast.css';
+//import Conditions from './components/Condition';
+//import WeatherComponent from './components/Weather';
+//import ApiComponent from './components/Kincardine';
+import Header from './views/shared/Header';
+import Landing from './views/Landing';
+import Forecast from './views/Forecast';
+import Live from './views/Live'
 
+
+//Dependencies
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-// practise
 
   return (
     <div className="App">
-      <ApiComponent />
-      <WeatherComponent />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route element={<Forecast />} />
-        </Routes>
-      </BrowserRouter> */}
-    {/* <Conditions />
-    <WeatherComponent />
-    <ApiComponent /> */}
+      <Header />
+    {/*<Conditions /> */}
+    {/*<WeatherComponent />*/}
+    {/*<ApiComponent />*/}
+    <BrowserRouter>
+          <Routes>
+            <Route path="/landing" element={<Landing/>}/>
+            <Route path="/forecast" element={<div id="forecast-page"><Forecast/></div>}/>
+            <Route path="/live" element={<Live/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ smartbeach-project-prototype/src/views/Forecast.js
@@ -0,0 +1,365 @@
+{/* <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Beach Project</title>
+    <link
+      rel="icon"
+      type="image/x-icon"
+      href="/assets/images/sunnyclowdy.png"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/forecast-style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+      integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+      crossorigin="anonymous"
+    ></script>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="javascript/functions.js" defer></script>
+  </head>
+  <body>
+    <header>
+      <div class="logo-container">
+        <img src="assets/images/logobeach.png" alt="Logo Image" id="hero" />
+      </div>
+    </header> */}
+    function Forecast() {
+      return(
+
+
+    <main>
+      <div class="forecast-table">
+        <table>
+          <caption class="d-flex justify-content-between align-items-center">
+            <h1 class="m-0">Forecast</h1>
+            <aside>
+              <i class="bi bi-caret-left"></i>
+              <i class="bi bi-caret-right"></i>
+            </aside>
+          </caption>
+          <tr class="days">
+            <th class="hidden"></th>
+            <th>Today <h1 class="headText">0.2m</h1><span class="green-bar"></span></th>
+            <th>Tommorow<h1 class="headText">0.4m</h1><span class="green-bar"></span></th>
+            <th>Thu<h1 class="headText">0.5m</h1><span class="green-bar"></span></th>
+            <th class="hidden">Thu<h1 class="headText">0.3m</h1><span class="green-bar"></span></th>
+          </tr>
+
+            <th class="mobile">
+              <i class="bi bi-three-dots"></i>
+            </th>
+
+          <tr class="mobile">
+            
+            <h2>Today<i class="bi bi-chevron-compact-down"></i></h2>
+            </tr>
+          
+          <tr class="mobile">
+            <th class="time">Time</th>
+            <th class="status">Status</th>
+            <th class="windM">Wind</th>
+            <th class="pressureM">Pressure</th>
+            <th class="weather">Weather</th>
+          </tr>
+
+          <tr class="mobile">
+            <th class="timed">6am</th>
+            <th class="statusd">Safe</th>
+            <th class="windd">32</th>
+            <th class="pressured">24</th>
+            <th class="weatherd">24</th>
+          </tr>
+
+          <tr class="mobile">
+            <th class="timed">6am</th>
+            <th class="statusd">Safe</th>
+            <th class="windd">32</th>
+            <th class="pressured">24</th>
+            <th class="weatherd">24</th>
+          </tr>
+
+          <tr class="mobile">
+            <th class="timed">6am</th>
+            <th class="statusd">Safe</th>
+            <th class="windd">32</th>
+            <th class="pressured">24</th>
+            <th class="weatherd">24</th>
+          </tr>
+
+          <tr class="wind">
+            <th>Wind Speed <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="surface">
+            <th >Global Surface <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="dpd">
+            <th>DPD<i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="mwd">
+            <th >MWD <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="pressure">
+            <th >Pressure <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="atmp">
+            <th>ATMP <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="wtmp">
+            <th >WTMP <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+
+          <tr class="dewp">
+            <th>DEWP <i class="bi bi-info-circle"></i></th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+            <th class="tableData">
+              <h4>1400</h4>
+
+              <span></span>
+              <span></span>
+              <span></span>
+            </th>
+          </tr>
+        </table>
+      </div>
+    </main>
+);
+}
+export default Forecast;
