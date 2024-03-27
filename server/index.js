6bb03185ce19e328042cbca036dfb5e9faf52a0b
WavyAirlines <imwavy4@gmail.com>
2024-03-27 11:59:45 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+let express = require('express');
+const app = express();  // this will create our express application
+const dotenv = require('dotenv');
+
+const bodyParser = require('body-parser');
+
+const conditions = require('./routes/conditions');
+const weather = require('./routes/weather');
+const kincardineApi = require('./routes/api');
+
+
+// const weather = require('/weather')
+app.use(bodyParser.json())
+
+
+app.use('/conditions', conditions);
+
+app.use('/weather', weather);
+
+app.use('/api', kincardineApi);
+
+app.get('/', async (req, res) => {
+    res.send('Server Running');
+});
+
+
+
+app.listen(8000)    // This is important as our react app will be using port 3000, however, we will need to move this to a .env
+
+module.exports = app;
